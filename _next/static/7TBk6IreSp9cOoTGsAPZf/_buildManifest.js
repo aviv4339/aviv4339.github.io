de1a18fca3a947eae192c9f6a7997c824db555c5
avive <34690623+aviv4339@users.noreply.github.com>
2022-12-10 19:12:15 +0000

    centered

--- _next/static/7TBk6IreSp9cOoTGsAPZf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/248-6eb7c8cd1f9b1e0e.js","static/chunks/pages/index-c6193e18f7f6e86f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
